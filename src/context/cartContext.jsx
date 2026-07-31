6363ecd4a358725b89eaa87846cd2d549f10637c
mastan shareef <mastanshareefshaik5@gmail.com>
2026-07-31 18:28:55 +0530

    initial modified

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -2,51 +2,81 @@ import { createContext, useState } from "react";
 
 export const CartContext = createContext();
 
+export function CartProvider({ children }) {
+  const [cartItems, setCartItems] = useState([]);
 
-export function CartProvider({children}){
+  const addToCart = (product) => {
+    setCartItems((previousItems) => {
+      const existingProduct = previousItems.find(
+        (item) => item.id === product.id
+      );
 
+      if (existingProduct) {
+        return previousItems.map((item) =>
+          item.id === product.id
+            ? {
+                ...item,
+                quantity: item.quantity + 1,
+              }
+            : item
+        );
+      }
 
-const [cartItems,setCartItems]=useState([]);
+      return [
+        ...previousItems,
+        {
+          ...product,
+          quantity: 1,
+        },
+      ];
+    });
+  };
 
+  const removeFromCart = (productId) => {
+    setCartItems((previousItems) =>
+      previousItems.filter((item) => item.id !== productId)
+    );
+  };
 
+  const increaseQuantity = (productId) => {
+    setCartItems((previousItems) =>
+      previousItems.map((item) =>
+        item.id === productId
+          ? {
+              ...item,
+              quantity: item.quantity + 1,
+            }
+          : item
+      )
+    );
+  };
 
-const addToCart=(product)=>{
+  const decreaseQuantity = (productId) => {
+    setCartItems((previousItems) =>
+      previousItems
+        .map((item) =>
+          item.id === productId
+            ? {
+                ...item,
+                quantity: item.quantity - 1,
+              }
+            : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
 
-setCartItems([
-...cartItems,
-product
-]);
-
-};
-
-
-
-const removeFromCart=(index)=>{
-
-const updatedItems = cartItems.filter(
-(_,i)=>i!==index
-);
-
-setCartItems(updatedItems);
-
-};
-
-
-
-return(
-
-<CartContext.Provider
-value={{
-cartItems,
-addToCart,
-removeFromCart
-}}
->
-
-{children}
-
-</CartContext.Provider>
-
-);
-
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        addToCart,
+        removeFromCart,
+        increaseQuantity,
+        decreaseQuantity,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 }
